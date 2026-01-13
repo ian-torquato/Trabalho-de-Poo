c6a4f6ea70ed694c1817af5c662881691111a893
moca1914 <moisesmdecarvalho@gmail.com>
2026-01-13 16:52:19 -0300

    classe da sobremesa

---
+++ sobremesa.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import java.util.List;
+
+public class Sobremesa extends Produto {
+
+    public Sobremesa(String nome, double preco, int tempoPreparo, List<String> restricoes) {
+        super(nome, preco, tempoPreparo, restricoes);
+    }
+
+    @Override
+    public String getCategoria() {
+        return "Sobremesa";
+    }
+}
