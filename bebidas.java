b3696022db2d82eca99755d6a1e323718392ac8f
moca1914 <moisesmdecarvalho@gmail.com>
2026-01-13 16:51:39 -0300

    classe das bebidas

---
+++ bebidas.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import java.util.List;
+
+public class Bebidas extends Produto {
+
+    public Bebidas(String nome, double preco, int tempoPreparo, List<String> restricoes) {
+        super(nome, preco, tempoPreparo, restricoes);
+    }
+
+    @Override
+    public String getCategoria() {
+        return "Bebidas";
+    }
+}
