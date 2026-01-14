8dcfa763f0af01f559c25276b5ec433bed6a44da
Ian Torquato <ian.torquato06@gmail.com>
2026-01-14 17:03:38 -0300

    Add exception handling for unavailable products

--- Pedido.java
+++ Pedido.java
@@ -1,6 +1,8 @@
 package modelo;
 
+import excecao.PedidoFinalizadoException;
 import excecao.PedidoVazioException;
+import excecao.ProdutoIndisponivelException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +24,9 @@ public class Pedido {
 
     public void adicionarItem(Produto produto, String comentario) {
 
+        if(!produto.isDisponivel()){
+            throw new ProdutoIndisponivelException();
+        }
         itens.add(new ItemPedido(produto, comentario));
     }
 
@@ -52,6 +57,9 @@ public class Pedido {
         if (itens.isEmpty()) {
             throw new PedidoVazioException();
         }
+        if(finalizado){
+            throw new PedidoFinalizadoException();
+        }
         finalizado = true;
     }
 
@@ -59,3 +67,4 @@ public class Pedido {
         return numero;
     }
 }
+
